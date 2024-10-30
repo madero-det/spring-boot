204e1dd23e6f6998b7d46c21b587db9b352b13cf
Madero Det <detmadero@gmail.com>
2024-10-30 02:13:31 +0000

    First commit

---
+++ validator-api/src/main/java/com/mcnc/validator/app/ValidatorApiApplication.java
@@ -0,0 +1,13 @@
+package com.mcnc.validator.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidatorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValidatorApiApplication.class, args);
+	}
+
+}
